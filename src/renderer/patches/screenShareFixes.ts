25b31cbaf7c65a6ad524d559eae10b69c5736082
Jeremy Zhang <jeremyzhang5000@gmail.com>
2024-04-23 22:48:45 -0700

    Soundshare fix

--- src/renderer/patches/screenShareFixes.ts
+++ src/renderer/patches/screenShareFixes.ts
@@ -10,60 +10,59 @@ import { isLinux } from "renderer/utils";
 
 const logger = new Logger("VesktopStreamFixes");
 
-if (isLinux) {
-    const original = navigator.mediaDevices.getDisplayMedia;
 
-    async function getVirtmic() {
-        try {
-            const devices = await navigator.mediaDevices.enumerateDevices();
-            const audioDevice = devices.find(({ label }) => label === "vencord-screen-share");
-            return audioDevice?.deviceId;
-        } catch (error) {
-            return null;
-        }
+const original = navigator.mediaDevices.getDisplayMedia;
+
+async function getVirtmic() {
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const audioDevice = devices.find(({ label }) => label.indexOf("Loopback") > -1);
+        return audioDevice?.deviceId;
+    } catch (error) {
+        return null;
+    }
+}
+
+navigator.mediaDevices.getDisplayMedia = async function (opts) {
+    const stream = await original.call(this, opts);
+    const id = await getVirtmic();
+
+    const frameRate = Number(currentSettings?.fps);
+    const height = Number(currentSettings?.resolution);
+    const width = Math.round(height * (16 / 9));
+    const track = stream.getVideoTracks()[0];
+
+    track.contentHint = String(currentSettings?.contentHint);
+
+    const constraints = {
+        ...track.getConstraints(),
+        frameRate,
+        width: { min: 640, ideal: width, max: width },
+        height: { min: 480, ideal: height, max: height },
+        advanced: [{ width: width, height: height }],
+        resizeMode: "none"
+    };
+
+    track
+        .applyConstraints(constraints)
+        .then(() => {
+            logger.info("Applied constraints successfully. New constraints: ", track.getConstraints());
+        })
+        .catch(e => logger.error("Failed to apply constraints.", e));
+
+    if (id) {
+        const audio = await navigator.mediaDevices.getUserMedia({
+            audio: {
+                deviceId: {
+                    exact: id
+                },
+                autoGainControl: false,
+                echoCancellation: false,
+                noiseSuppression: false
+            }
+        });
+        audio.getAudioTracks().forEach(t => stream.addTrack(t));
     }
 
-    navigator.mediaDevices.getDisplayMedia = async function (opts) {
-        const stream = await original.call(this, opts);
-        const id = await getVirtmic();
-
-        const frameRate = Number(currentSettings?.fps);
-        const height = Number(currentSettings?.resolution);
-        const width = Math.round(height * (16 / 9));
-        const track = stream.getVideoTracks()[0];
-
-        track.contentHint = String(currentSettings?.contentHint);
-
-        const constraints = {
-            ...track.getConstraints(),
-            frameRate,
-            width: { min: 640, ideal: width, max: width },
-            height: { min: 480, ideal: height, max: height },
-            advanced: [{ width: width, height: height }],
-            resizeMode: "none"
-        };
-
-        track
-            .applyConstraints(constraints)
-            .then(() => {
-                logger.info("Applied constraints successfully. New constraints: ", track.getConstraints());
-            })
-            .catch(e => logger.error("Failed to apply constraints.", e));
-
-        if (id) {
-            const audio = await navigator.mediaDevices.getUserMedia({
-                audio: {
-                    deviceId: {
-                        exact: id
-                    },
-                    autoGainControl: false,
-                    echoCancellation: false,
-                    noiseSuppression: false
-                }
-            });
-            audio.getAudioTracks().forEach(t => stream.addTrack(t));
-        }
-
-        return stream;
-    };
-}
+    return stream;
+};
